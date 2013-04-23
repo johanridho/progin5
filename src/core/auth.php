7af48f38dfeaa77c099bd2d436b3dd8bc7693343
Irfan Kamil <kmz.in.act@gmail.com>
2013-04-23 15:10:42 +0700

    Implemented API Function

--- src/core/auth.php
+++ src/core/auth.php
@@ -1,14 +1,13 @@
 <?php
 	require_once 'config.php';
-	$get = $_GET;
-	$username = $get['username'];
-	$password = $get['password'];
-	$hasil = query('SELECT * FROM user WHERE username = :username and password = :password',array('username' => $username, 'password' => $password));
-	if ($hasil) {
-		$arr['success'] = true;
-		$arr['user_id'] = (int)$hasil['user_id'];
-		$_SESSION['user_id'] = (int)$hasil['user_id'];
-	} else
-		$arr['success'] = false;
-	echo json_encode($arr);
+	function auth($username,$password) {
+		$hasil = query('SELECT * FROM user WHERE username = :username and password = :password',array('username' => $username, 'password' => $password));
+		if ($hasil) {
+			$arr['success'] = true;
+			$arr['user_id'] = (int)$hasil['user_id'];
+			$_SESSION['user_id'] = (int)$hasil['user_id'];
+		} else
+			$arr['success'] = false;
+		echo json_encode($arr);
+	}
 ?>
--- src/core/countComment.php
+++ src/core/countComment.php
@@ -1,7 +1,7 @@
 <?php
 	require_once 'config.php';
-	$get = $_GET;
-	$task_id = $get['task_id'];
-	$hasil = query('select count(*) from comment WHERE task_id = :task_id',array('task_id' => $task_id));
-	echo $hasil['count(*)'];
+	function countComment($task_id) {
+		$hasil = query('select count(*) from comment WHERE task_id = :task_id',array('task_id' => $task_id));
+		echo $hasil['count(*)'];
+	}
 ?>
