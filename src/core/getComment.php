68da2fe5bd6a904d8fa3b7fa834f8e1a0a6cd2ba
Irfan Kamil <kmz.in.act@gmail.com>
2013-05-01 07:49:53 +0700

    fixed get comment

--- src/core/getComment.php
+++ src/core/getComment.php
@@ -1,10 +1,7 @@
 <?php
 	require_once 'config.php';
 	function getComment($task_id, $offset) {
-		$hasil = queryAll('select * from comment WHERE task_id = :task_id order by time asc limit '.$offset.', 10',array('task_id' => $task_id));
-		foreach ($hasil as &$row) {
-			$row['user_name'] = getUserName($row['user_id']);
-		}
+		$hasil = queryAll('select comment.*,name from (select * from comment WHERE task_id = :task_id order by time asc limit '.$offset.', 10) as comment natural join user',array('task_id' => $task_id));
 		echo json_encode($hasil);
 	}
 ?>
