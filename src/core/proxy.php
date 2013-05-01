62f012b0077ab543cb21f16f00c35b9bf9af90cc
Irfan Kamil <kmz.in.act@gmail.com>
2013-05-01 07:50:10 +0700

    proxy fixed

--- src/core/proxy.php
+++ src/core/proxy.php
@@ -1,7 +1,13 @@
 <?php
-// File Name: proxy.php
-if (!isset($_GET['url'])) die();
-$url = urldecode($_GET['url']);
-$url = 'http://' . str_replace('http://', '', $url); // Avoid accessing the file system
-echo file_get_contents($url);
+	// File Name: proxy.php
+	if (!isset($_GET['url'])) die();
+	$url = urldecode($_GET['url']);
+	$url = 'http://' . str_replace(' ','%20',str_replace('http://', '', $url)); // Avoid accessing the file system
+	$method = $_SERVER['REQUEST_METHOD'];
+	$context = stream_context_create(array(
+		'http' => array(
+			'method' => $method
+		)
+	));
+	echo file_get_contents($url,false,$context);
 ?>
